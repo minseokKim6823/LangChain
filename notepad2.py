f3d9847dfb9719721186b9a0b73fa6cc48ba5a46
minseokkim6823 <alstjr6823@naver.com>
2026-05-15 10:23:59 +0900

    길이에 맞춰 청킹

--- notepad2.py
+++ notepad2.py
@@ -3,8 +3,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 urllib.request.urlretrieve("https://raw.githubusercontent.com/lovit/soynlp/master/tutorials/2016-10-20.txt", filename="2016-10-20.txt")
 
+# RecursiveCharacterTextSplitter 길이에 맞추어 분할
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-
 with open("2016-10-20.txt", encoding="utf-8") as f:
     file = f.read()
 # print('텍스트의 길이:', len(file))
@@ -12,4 +12,11 @@ with open("2016-10-20.txt", encoding="utf-8") as f:
 texts = text_splitter.create_documents([file])
 # print('분할된 청크의 수:', len(texts))
 
-print('첫 번째 청크의 내용:', texts[1])
+#Document(page_content="텍스트")의 형태
+print(texts[1])
+#원문 출력
+print(texts[1].page_content)
+
+print('1번 청크의 길이:', len(texts[1].page_content))
+print('2번 청크의 길이:', len(texts[2].page_content))
+
